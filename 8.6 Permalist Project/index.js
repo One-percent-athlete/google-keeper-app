46949d60ae1e0ac1e4085620687545bb6089e47b
Ryu Suzuki <ryu.super.s@gmail.com>
2023-11-14 10:50:49 +0900

    first commit

---
+++ 8.6 Permalist Project/index.js
@@ -0,0 +1,61 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "onepercentathlete",
+  port: 5433,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: result.rows,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+
+  await db.query(
+    "INSERT INTO items (title) VALUES ($1)",
+    [item]
+  );
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+    const id = req.body.updatedItemId;
+    const title = req.body.updatedItemTitle;
+
+  await db.query("UPDATE items SET title = $1 WHERE id = $2", 
+  [title, id]);
+
+  res.redirect("/");
+});
+
+app.post("/delete", async (req, res) => {
+  const deleteId = req.body.deleteItemId
+
+  await db.query("DELETE FROM items WHERE id= $1", 
+  [deleteId]);
+
+  res.redirect("/");
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
